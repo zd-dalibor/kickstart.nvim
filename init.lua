fa154787e1e4fb3a2c91bf0c9135d9ffd40e0ec0
z_dalibor <zd.dalibor@gmail.com>
2025-04-21 12:37:51 +0200

    Use catpuccing theme

--- init.lua
+++ init.lua
@@ -986,7 +986,7 @@ require('lazy').setup({
         },
       }
 
-      -- vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
   {
@@ -1007,7 +1007,7 @@ require('lazy').setup({
 
       -- vim.cmd.colorscheme 'github_dark_colorblind'
       -- vim.cmd.colorscheme 'github_dark_dimmed'
-      vim.cmd.colorscheme 'github_dark_default'
+      -- vim.cmd.colorscheme 'github_dark_default'
     end,
   },
 
