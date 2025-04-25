9f822726a5c8216d8b4d8863fb9250104361bc64
z_dalibor <zd.dalibor@gmail.com>
2025-04-25 22:11:30 +0200

    Disable not used themes

--- init.lua
+++ init.lua
@@ -965,6 +965,7 @@ require('lazy').setup({
   },
   {
     'olimorris/onedarkpro.nvim',
+    enabled = false,
     priority = 1000,
     config = function()
       require('onedarkpro').setup {
@@ -993,11 +994,12 @@ require('lazy').setup({
       }
 
       -- require('vscode').load 'dark'
-      -- vim.cmd.colorscheme 'vscode'
+      vim.cmd.colorscheme 'vscode'
     end,
   },
   {
     'catppuccin/nvim',
+    enabled = false,
     name = 'catppuccin',
     priority = 1000,
     config = function()
@@ -1018,8 +1020,8 @@ require('lazy').setup({
   },
   {
     'projekt0n/github-nvim-theme',
+    enabled = false,
     name = 'github-theme',
-    lazy = false,
     priority = 1000,
     config = function()
       require('github-theme').setup {
@@ -1042,7 +1044,7 @@ require('lazy').setup({
   },
   {
     'rebelot/kanagawa.nvim',
-    lazy = false,
+    enabled = false,
     priority = 1000,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -1057,12 +1059,35 @@ require('lazy').setup({
         end,
       }
       -- vim.cmd.colorscheme 'kanagawa-wave'
-      vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+  },
+  {
+    'thesimonho/kanagawa-paper.nvim',
+    enabled = false,
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('kanagawa-paper').setup {
+        styles = {
+          comment = { italic = false },
+          type = { italic = false },
+        },
+        overrides = function()
+          return {
+            DiagnosticUnderlineHint = { underdotted = true },
+          }
+        end,
+      }
+
+      -- vim.cmd.colorscheme 'kanagawa-paper'
+      -- vim.cmd.colorscheme 'kanagawa-paper-ink'
+      -- vim.cmd.colorscheme 'kanagawa-paper-canvas'
     end,
   },
   {
     'morhetz/gruvbox',
-    lazy = false,
+    enabled = false,
     priority = 1000,
     config = function()
       -- vim.cmd.colorscheme 'gruvbox'
