b42b3c6786b94b9c9694032241627c2eacadf2ed
z_dalibor <zd.dalibor@gmail.com>
2025-05-03 10:57:54 +0200

    Theme settings

--- init.lua
+++ init.lua
@@ -84,8 +84,8 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- Set <space> as the leader key
@@ -903,7 +903,6 @@ require('lazy').setup({
   },
   {
     'olimorris/onedarkpro.nvim',
-    enabled = false,
     priority = 1000,
     config = function()
       require('onedarkpro').setup {
@@ -914,7 +913,7 @@ require('lazy').setup({
         },
       }
 
-      -- vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'onedark'
       -- vim.cmd.colorscheme 'onelight'
       -- vim.cmd.colorscheme 'onedark_vivid'
       -- vim.cmd.colorscheme 'onedark_dark'
@@ -927,12 +926,14 @@ require('lazy').setup({
       require('vscode').setup {
         disable_nvimtree_bg = true,
         group_overrides = {
-          DiagnosticUnderlineHint = { underdotted = true },
+          DiagnosticUnderlineHint = {
+            underdotted = true,
+          },
         },
       }
 
       -- require('vscode').load 'dark'
-      vim.cmd.colorscheme 'vscode'
+      -- vim.cmd.colorscheme 'vscode'
     end,
   },
   {
